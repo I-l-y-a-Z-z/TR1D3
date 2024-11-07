8871ff897f1943247d457a109ab26238f6242a3b
Ilyas Rahmouni <elrahmouni.ilyas06@gmail.com>
2024-11-07 10:26:36 +0100

    Update NavBar.tsx

--- LetsTradeUM6P/TR4D3/LetsTradeUM6P/src/components/NavBar.tsx
+++ LetsTradeUM6P/TR4D3/LetsTradeUM6P/src/components/NavBar.tsx
@@ -48,12 +48,12 @@ function NavBar() {
                 </Link>
                 <ul className="dropdown-menu">
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="/explore">
                       Food
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="/explore">
                       Clothing
                     </Link>
                   </li>
@@ -61,17 +61,17 @@ function NavBar() {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="/explore">
                       Electronics
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="/explore">
                       Books
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" href="#">
+                    <Link className="dropdown-item" href="/explore">
                       Other
                     </Link>
                   </li>
